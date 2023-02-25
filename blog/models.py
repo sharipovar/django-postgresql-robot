c0f0ea6c2fe616d0f1f465a6fd8843c4a6ab8726
Raisa Sharipova <sharipova_rk@mail.ru>
2023-02-25 16:43:39 +0400

    создание миграции из моделей

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Topic(models.Model):
     topic_name = models.CharField(max_length=200)
 
 class Message(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
     created_at = models.DateTimeField('date published')
