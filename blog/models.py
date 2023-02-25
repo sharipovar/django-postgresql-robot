4367c78a345aaaa024627bef0a2500a0e4eeadfb
Raisa Sharipova <sharipova_rk@mail.ru>
2023-02-25 16:09:55 +0400

    создаю модели для таблиц User, Topic, Message

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=200)
+
+class Topic(models.Model):
+    topic_name = models.CharField(max_length=200)
+
+class Message(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    created_at = models.DateTimeField('date published')
