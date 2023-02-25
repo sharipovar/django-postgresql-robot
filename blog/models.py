0026353263a182f7f4ec4570d501c7f0b42b5c3d
Raisa Sharipova <sharipova_rk@mail.ru>
2023-02-25 17:13:08 +0400

    делаю строковый вывод полей модели

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,14 @@ from django.db import models
 class User(models.Model):
     user_name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return str(self.id) + " " + self.user_name
+
 class Topic(models.Model):
     topic_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.id) + " " + self.topic_name
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
